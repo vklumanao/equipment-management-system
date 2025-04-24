90bf31e1e97912b4c52cf04ef977e62d562a31dd
lesley <flores.lesleyrose01@gmail.com>
2025-04-24 23:21:43 +0800

    added ui design for the driver dashboard view

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import DashboardView from '@/views/system/DashboardView.vue'
+import DriverView from '@/views/system/DriverView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView,
+    },
+    {
+      path: '/dashboard/driver',
+      name: 'driver',
+      component: DriverView,
     },
   ],
 })
